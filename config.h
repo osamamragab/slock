e2a292f6ac5525d5dd6a2046fbe1a8c12e7b4eab
Osama <iosamaify@gmail.com>
2022-05-21 22:28:15 +0200

    apply failure command patch

--- config.h
+++ config.h
@@ -3,8 +3,8 @@ static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] = "#1c1e1f",   /* after initialization */
-	[INPUT] = "#005577",   /* during input */
+	[INIT]   = "#1c1e1f",   /* after initialization */
+	[INPUT]  = "#005577",   /* during input */
 	[FAILED] = "#cc3333",   /* wrong password */
 };
 
@@ -22,3 +22,10 @@ static const char *text_color = "#ffffff";
 
 /* text size (must be a valid size) */
 static const char *font_name = "10x20";
+
+/* number of failed password attempts until failcommand is executed.
+   Set to 0 to disable */
+static const int failcount = 5;
+
+/* command to be executed after [failcount] failed password attempts */
+static const char *failcommand = "doas poweroff";
