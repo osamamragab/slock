85b4ccddb662b2e45761c9ca28c718b4f971b380
Osama Ragab <theosamaragab@gmail.com>
2023-07-25 20:34:30 +0300

    Revert "add message patch"
    This reverts commit 24f316302dfe5c4f80dddcd5dc4d893c9a0adcd8.

--- config.def.h
+++ config.def.h
@@ -1,11 +1,11 @@
 /* user and group to drop privileges to */
-static const char *user = "nobody";
+static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-    [INIT] = "black",     /* after initialization */
-    [INPUT] = "#005577",  /* during input */
-    [FAILED] = "#CC3333", /* wrong password */
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#005577",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
@@ -16,12 +16,3 @@ static const int failbell = 1;
 
 /* time in seconds before the monitor shuts down */
 static const int monitortime = 5;
-
-/* default message */
-static const char *message = "Suckless: Software that sucks less.";
-
-/* text color */
-static const char *text_color = "#ffffff";
-
-/* text size (must be a valid size) */
-static const char *font_name = "6x10";
